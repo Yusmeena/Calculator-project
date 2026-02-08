e8f47ccf6541ec77e088bd6c6a6578a86f5d57db
Ojo Yusuf Abdulzeez <121867692+Yusmeena@users.noreply.github.com>
2026-02-08 06:32:59 +0100

    Add files via upload
    Using HTML CSS and JavaScript to design calculator

---
+++ calculator.js
@@ -0,0 +1,75 @@
+function Calculate(){
+    const value0 = document.getElementById("number0").textContent;
+    const displayInput = document.getElementById("displayInput").textContent= value0;
+    console.log(displayInput)
+}
+
+// try{
+    
+// }
+
+// catch (error){
+//     console.log(error)
+// }
+
+// function Calculate(){
+    
+//     let number7 = document.getElementById("number7").textContent;
+//     console.log(number7)
+
+//     let number8 = document.getElementById("number8").textContent;
+//     console.log(number8)
+//     const userValue1 = document.getElementById("UserValue1").innerHTML = number7;
+//     // console.log(userValue1)
+    
+//     }
+
+const value0 = document.getElementById("number0").textContent;
+// const value1 = document.getElementById("number1").textContent;
+// const value2 = document.getElementById("number2").textContent;
+// const value3 = document.getElementById("number3").textContent;
+// const value4 = document.getElementById("number4").textContent;
+// const value5 = document.getElementById("number5").textContent;
+// const value6 = document.getElementById("number6").textContent;
+// const value7 = document.getElementById("number7").textContent;
+// const value8 = document.getElementById("number8").textContent;
+// const value9 = document.getElementById("number9").textContent;
+// const point = document.getElementById("point").textContent;
+
+
+
+
+// function Calculate(checkButton){
+//     if (value0){
+//         checkButton = value0
+//         console.log(checkButton)
+//     }else if(value1){
+//         checkButton = value1;
+//         console.log(checkButton);
+//     }
+    
+//     const userValue1 = document.getElementById("UserValue1").innerHTML = value0;
+//     // console.log(userValue1)
+
+    
+    
+    
+//     }
+
+
+
+
+
+// function Zero(){
+//     const value0 = document.getElementById("number0").textContent;
+//     console.log(value0)
+// }
+// function One(){
+//     const value1 = document.getElementById("number1").textContent;
+//     console.log(value1)
+// }
+
+// function Two(){
+//     const value2 = document.getElementById("number2").textContent;
+//     console.log(value2)
+// }
